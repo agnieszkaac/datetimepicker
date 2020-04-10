1e7888cbcb342bf4afe0ed1c927aa0c65563b714
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2020-04-10 21:53:36 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+
+import "./App.scss";
+
+class App extends React.PureComponent {
+  render() {
+    return <>App</>;
+  }
+}
+export default hot(App);
---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, "src"),
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+});
