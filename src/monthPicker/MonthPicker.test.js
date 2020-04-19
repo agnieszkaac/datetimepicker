3a3e59fa8b17c1fb7937da8c80e4e8bf76a32049
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2020-04-19 18:54:12 +0200

    Update Month tests

--- src/monthPicker/MonthPicker.test.js
+++ src/monthPicker/MonthPicker.test.js
@@ -1,48 +1,58 @@
 import React from "react";
 import { render } from "@testing-library/react";
-
-import { Month, MonthPicker } from "./";
 import moment from "moment";
 
+import { Month, MonthPicker } from "./";
+import { DayPicker } from "../dayPicker";
+
 const onPick = jest.fn();
-jest.mock("moment", () => (value) => ({
-  month: (i) => "1",
-  format: () => "Jan",
-  isSame: (value, granularity) => true,
-  toISOString: () => "",
-}));
+Date.now = jest.fn(() => Date.parse("2020-01-01"));
 
 describe("<Month/> should", () => {
-  it("math snapshot for NOT present month, NOT selected", () => {
-    const { container } = render(
-      <Month date={"2"} selected={false} onPick={onPick} />,
-    );
-    expect(container).toMatchSnapshot();
+  describe("match snapshot for present day", () => {
+    it("and NOT selected", () => {
+      const { container } = render(
+        <Month date={moment()} selected={false} onPick={onPick} />,
+      );
+      expect(container).toMatchSnapshot();
+    });
+    it("and selected", () => {
+      const { container } = render(
+        <Month date={moment()} selected={true} onPick={onPick} />,
+      );
+      expect(container).toMatchSnapshot();
+    });
   });
-  it("match snapshot for NOT present month, selected", () => {
-    const { container } = render(
-      <Month date={"2"} selected={true} onPick={onPick} />,
-    );
-    expect(container).toMatchSnapshot();
-  });
-  it("match snapshot for present month, NOT selected", () => {
-    const { container } = render(
-      <Month date={"1"} selected={false} onPick={onPick} />,
-    );
-    expect(container).toMatchSnapshot();
-  });
+  describe("match snapshot for NOT present day", () => {
+    it("and NOT selected", () => {
+      const { container } = render(
+        <Month
+          date={moment().add(1, "month")}
+          selected={false}
+          onPick={onPick}
+        />,
+      );
+      expect(container).toMatchSnapshot();
+    });
 
-  it("match snapshot for present month, selected", () => {
-    const { container } = render(
-      <Month date={"1"} selected={true} onPick={onPick} />,
-    );
-    expect(container).toMatchSnapshot();
+    it("and selected", () => {
+      const { container } = render(
+        <Month
+          date={moment().add(1, "month")}
+          selected={true}
+          onPick={onPick}
+        />,
+      );
+      expect(container).toMatchSnapshot();
+    });
   });
 });
 
 describe("<MonthPicker /> should", () => {
   it("match snapshot", () => {
-    const { container } = render(<MonthPicker onPick={onPick} />);
+    const { container } = render(
+      <MonthPicker date={moment()} viewDate={moment()} onPick={onPick} />,
+    );
     expect(container).toMatchSnapshot();
   });
 });
