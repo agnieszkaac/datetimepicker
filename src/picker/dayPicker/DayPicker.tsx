47cf72d3b1a7d0cb00398f51bca9e1a6e8b0f36d
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2020-04-20 23:29:46 +0200

    WIP

--- src/picker/dayPicker/DayPicker.tsx
+++ src/picker/dayPicker/DayPicker.tsx
@@ -3,6 +3,8 @@ import moment from "moment";
 
 import { DayPickerProps, Day } from "./index";
 
+const days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+
 export const DayPicker: React.FunctionComponent<DayPickerProps> = ({
   date,
   viewDate,
@@ -10,14 +12,23 @@ export const DayPicker: React.FunctionComponent<DayPickerProps> = ({
 }) => {
   return (
     <div className="day-picker">
-      {Array.from({ length: moment(viewDate).daysInMonth() }, (_e, i) => (
-        <Day
-          key={i + 1}
-          date={moment(viewDate).date(i + 1)}
-          selected={date?.date() === i + 1 && date?.isSame(viewDate, "month")}
-          onPick={onPick}
-        />
+      {Array.from({ length: days.length }, (_e, i) => (
+        <div key={i} className="day">{days[i]}</div>
       ))}
+      {Array.from({ length: moment(viewDate).daysInMonth() }, (_e, i) => {
+        let showDate = moment(viewDate).date(i+1);
+        if(moment(viewDate).date(i+1).day() > 0) {
+          showDate = moment(viewDate).subtract(moment(viewDate).date(i+1).day(), "day")
+        }
+        return (
+          <Day
+            key={i + 1}
+            date={moment(viewDate).date(i + 1)}
+            selected={date?.date() === i + 1 && date?.isSame(viewDate, "month")}
+            onPick={onPick}
+          />
+        )
+      })}
     </div>
   );
 };
