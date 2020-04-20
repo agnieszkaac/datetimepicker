eefab173af96c9c90ca63afa6e0b13a986d16dca
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2020-04-20 20:39:00 +0200

    Create picker click listener

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import moment, { Moment } from "moment";
 
 import { DateType, MomentDate } from "./types";
@@ -20,3 +21,15 @@ export const getStringDate = (value: MomentDate, format: string): string => {
 
 export const getDecadeStart = (date: Moment) =>
   Math.floor(moment(date).get("year") / 10) * 10;
+
+export const togglePickerOpen = (
+  pickerOpen: boolean,
+  handleClick: (e: MouseEvent) => void,
+) => {
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, [pickerOpen]);
+};
